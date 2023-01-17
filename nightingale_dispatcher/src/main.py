c3650e2deeb14bbb067840fc8f5abede4b98841f
le2tang <le2tang@uwaterloo.ca>
2023-01-17 17:40:30 -0500

    Add main.py

--- nightingale_dispatcher/src/main.py
+++ nightingale_dispatcher/src/main.py
@@ -2,13 +2,21 @@
 
 import rospy
 
+import queue
+
 
 class TaskManager:
-    def __init__(self):
-        self.tasks = {}
+    def __init__(self, maxsize=0):
+        self.tasks = queue.PriorityQueue(maxsize)
 
 
 class Task:
+    SUCCESS = 0
+    ARGUMENT_ERROR = 1
+    SERVICE_ERROR = 2
+    TOPIC_ERROR = 3
+    ACTION_ERROR = 4
+
     def __init__(self, name, priority=0):
         self.name = name
         self.priority = priority
@@ -29,18 +37,25 @@ class NavigateTask(Task):
         self.lookup_proxy = rospy.ServiceProxy("/lookup_room_pose", RoomLookup)
 
     def execute(self):
+        res = self._try_lookup_room()
+        if res != self.SUCCESS:
+            return res
+
         
-    
 
     def _try_lookup_room(self):
         try:
             res = self.lookup_proxy(self.room, self.feature)
             
-            if res.status == RoomPoseLookupResponse.FEATURE_NOT_FOUND
+            if res.status == RoomPoseLookupResponse.ROOM_NOT_FOUND:
+                return self.ARGUMENT_ERROR
+            elif res.status == RoomPoseLookupResponse.FEATURE_NOT_FOUND:
+                return self.ARGUMENT_ERROR
+            return self.SUCCESS
 
         except rospy.ServiceException as e:
             print(f"Room Lookup service call failed: {e}")
-            return False
+            return self.SERVICE_ERROR
 
 
 class DispatcherNode:
