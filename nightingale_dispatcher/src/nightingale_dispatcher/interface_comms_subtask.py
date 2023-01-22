ca126e3e1b8b749ba1ac182e31d3ad14ed0c664e
Felix Wong <felix.wongkh@outlook.com>
2023-01-21 22:51:53 -0500

    Move state machine out of comms task

--- nightingale_dispatcher/src/nightingale_dispatcher/interface_comms_subtask.py
+++ nightingale_dispatcher/src/nightingale_dispatcher/interface_comms_subtask.py
@@ -3,7 +3,7 @@
 import rospy
 
 from nightingale_dispatcher.subtask import Subtask
-from nightingale_ros_bridge import RobotStatus, BridgeConfig, UserInputs
+from nightingale_ros_bridge import BridgeConfig 
 
 class InterfaceCommsSubtask(Subtask):
     def __init__(self, name, robot_state, priority):
@@ -18,35 +18,8 @@ class InterfaceCommsSubtask(Subtask):
 
     def execute(self):
         try:
+            # extracts user input for task manager to handle
             res = self.interface_comms_proxy(self.robot_state)
-
-            # state machine for interface responses
-            # TODO replace with actual input codes
-            if res.user_input == UserInputs.NO_INPUT:
-                # do nothing, just updating patient status
-                pass
-            elif res.user_input == UserInputs.STOCK_ITEMS:
-                # start up navigation task with goal of STOCK 
-                pass
-            elif res.user_input == UserInputs.DELIVER_ITEMS:
-                # start up navigation task with goal of PATIENT ROOM
-                pass
-            elif res.user_input == UserInputs.RETURN_HOME:
-                # start up navigation task with goal of HOME
-                pass
-            elif res.user_input == UserInputs.START_EXTEND_ARM:
-                # start up manipulation task with arm goal of DELIVERY
-                pass
-            elif res.user_input == UserInputs.ITEMS_TAKEN:
-                # intput to let node know the patient status is during arm delivery 
-                pass
-            elif res.user_input == UserInputs.START_RETRACT_ARM:
-                # start up manipulation task with arm goal of ARM_HOME
-                pass
-            elif res.user_input == UserInputs.WD_TIMEOUT:
-                # start up navigation task with goal of HOME. Quit other tasks related to patient
-                pass
-
             return res.user_input
 
         except rospy.ServiceException as e:
--- nightingale_dispatcher/src/nightingale_dispatcher/task_action_server.py
+++ nightingale_dispatcher/src/nightingale_dispatcher/task_action_server.py
@@ -6,6 +6,7 @@ import time
 
 from actionlib_msgs.msg import GoalStatus
 from nightingale_msgs.msg import TaskAction
+from nightingale_ros_bridge import RobotStatus, UserInputs
 
 
 class TaskActionServer:
@@ -17,11 +18,46 @@ class TaskActionServer:
         )
         self.server.start()
 
+        self.current_robot_state = RobotStatus.IDLE_HOME
+
     def goal_cb(self, goal):
         # TODO execute subtasks in order and report status
         # Nav -> Bed. idle -> Nav -> Stock -> Bed. deliver -> Nav -> Idle
 
         self.server.set_succeeded()
+
+    def process_user_input(user_input):
+        """
+        takes in the enum of user input codes and returns an enum based on what robot state should come next and what task is to be queued 
+        """
+        next_robot_state = self.current_robot_state 
+        # state machine for interface responses
+        if user_input == UserInputs.NO_INPUT:
+            # do nothing, just updating patient status
+            pass
+        elif user_input == UserInputs.STOCK_ITEMS:
+            # start up navigation task with goal of STOCK 
+            next_robot_state = RobotStatus.STOCK_ITEMS 
+        elif user_input == UserInputs.DELIVER_ITEMS:
+            # start up navigation task with goal of PATIENT ROOM
+            next_robot_state = RobotStatus.DELIVER_ITEMS 
+        elif user_input == UserInputs.RETURN_HOME:
+            # start up navigation task with goal of HOME
+            next_robot_state = RobotStatus.DRIVING 
+        elif user_input == UserInputs.START_EXTEND_ARM:
+            # start up manipulation task with arm goal of DELIVERY
+            next_robot_state = RobotStatus.EXTENDING_ARM 
+        elif user_input == UserInputs.ITEMS_TAKEN:
+            # intput to let node know the patient status is during arm delivery 
+            next_robot_state = RobotStatus.ARM_EXTENDED 
+        elif user_input == UserInputs.START_RETRACT_ARM:
+            # start up manipulation task with arm goal of ARM_HOME
+            next_robot_state = RobotStatus.RETRACTING_ARM 
+        elif user_input == UserInputs.WD_TIMEOUT:
+            # start up navigation task with goal of HOME. Quit other tasks related to patient
+            next_robot_state = RobotStatus.DRIVING 
+
+        return next_robot_state
 
 
 def main():
