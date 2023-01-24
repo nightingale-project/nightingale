84446be9d8a5c222d4cea997e66302a2d2103fbe
Younes Reda <younesreda1@outlook.com>
2023-01-24 18:52:24 -0500

    test script: current state

--- debug/src/debug_action_client.py
+++ debug/src/debug_action_client.py
@@ -23,7 +23,7 @@ def moveit_tojoints_client():
     point = JointTrajectoryPoint()
     point.positions = [1.8055517131984544, 0.7156651544664865, 0.4545099853350272, 1.2894898166431348,
                        -3.012074075031533, 0.9220841073974382, -2.4957912692637914]
-    point.time_from_start.secs = 20
+    point.time_from_start.secs = 3
     goal.trajectory.points.append(point)
 
     goal.trajectory.joint_names = ["right_shoulder_pan_joint",
@@ -37,8 +37,8 @@ def moveit_tojoints_client():
     new_goal = FollowJointTrajectoryGoal()
     new_point = JointTrajectoryPoint()
     new_point.positions = [2.2899584780887663, 0.6968041149943274, -0.4708056683081403, 1.4458962021990525,
-                           2.523168565269806, 0.9773449663326914, -2.494001358695149]
-    new_point.time_from_start.secs = 20
+                           -2.523168565269806, 0.9773449663326914, -2.494001358695149]
+    new_point.time_from_start.secs = 3
     new_goal.trajectory.points.append(new_point)
 
     new_goal.trajectory.joint_names = ["right_shoulder_pan_joint",
