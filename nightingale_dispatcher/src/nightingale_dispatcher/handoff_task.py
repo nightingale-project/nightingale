08cb1a29c587897c2316998c157fcae662ec45b2
Lyndon Tang <44735835+le2tang@users.noreply.github.com>
2023-01-30 14:58:01 -0500

    User/le2tang/dispatcher0 (#23)
    * Add room lookup service
    
    * Add room poses for current map
    
    * Fix formatting
    
    * Add spaces
    
    * Fix service naming
    
    * Move lookup to nightingale_utils
    
    * Add dispatcher package
    
    * Add main.py
    
    * Move files to package
    
    * Refactor to use action library for tasks
    
    * Fix formatting
    
    * Fix missing space
    
    * Rename tasks
    
    * Add task files
    
    * Add states that use tasks
    
    * Fix runtime error
    
    * Add more detailed log info to action server
    
    * Fix formatting

---
+++ nightingale_dispatcher/src/nightingale_dispatcher/handoff_task.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from nightingale_dispatcher.task import Task
+
+
+class HandoffTask(Task):
+    # Once arrived at the bedside, pass of items to patient
+    # TODO define parameters
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    # TODO define inputs
+    def execute(self, *args, **kwargs):
+        # look for patient
+        # show screen
+        # move box to patient
+        raise NotImplementedError()
---
+++ nightingale_dispatcher/src/nightingale_dispatcher/mission_planner.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import rospy
+import actionlib
+import queue
+
+from actionlib_msgs.msg import GoalStatus
+from std_msgs.msg import String
+from nightingale_msgs.msg import MissionPlanAction
+from nightingale_dispatcher.handoff_task import HandoffTask
+from nightingale_dispatcher.idle_task import IdleTask
+from nightingale_dispatcher.navigate_task import NavigateTask
+from nightingale_dispatcher.stock_task import StockTask
+from nightingale_dispatcher.task import Task
+from nightingale_dispatcher.triage_task import TriageTask
+from nightingale_ros_bridge.bridge_interface_config import BridgeConfig
+
+
+class MissionPlanner:
+    def __init__(self):
+        rospy.init_node("mission_planner_node")
+
+        self.estop_sub = rospy.Subscriber(
+            BridgeConfig.USER_INPUT_TOPIC, String, self.estop_cb
+        )
+
+        self.server = actionlib.SimpleActionServer(
+            "mission_planner", MissionPlanAction, self.goal_cb, False
+        )
+        self.server.start()
+
+        self.handoff_task = HandoffTask()
+        self.idle_task = IdleTask()
+        self.navigate_task = NavigateTask()
+        self.stock_task = StockTask()
+        self.triage_task = TriageTask()
+
+        self.phases = queue.Queue()
+
+    def go_to_patient(self):
+        # Extract room number and bed number from goal (bell)
+        status = self.navigate_task.execute(self.room, "bed")
+        if status == Task.ERROR:
+            raise NotImplementedError()
+        self.phases.put(self.triage_patient)
+
+    def triage_patient(self):
+        # Arrived at patient's bedside
+        status = self.triage_task.execute()
+        if status == Task.ERROR:
+            raise NotImplementedError()
+
+        if status == TriageTask.TIMEOUT:
+            # User didn't want anything
+            self.phases.put(self.go_idle)
+        else:
+            # User wants some items
+            self.phases.put(self.go_to_stock)
+
+    def go_to_stock(self):
+        # Go to stock room
+        status = self.navigate_task.execute("stock")
+        if status == Task.ERROR:
+            raise NotImplementedError()
+        self.phases.put(self.get_items)
+
+    def get_items(self):
+        # Arrived at stock area
+        status = self.stock_task.execute()
+        if status == Task.ERROR:
+            raise NotImplementedError()
+        self.phases.put(self.return_to_patient)
+
+    def return_to_patient(self):
+        # Got items, go back to patient room
+        status = self.navigate_task.execute(self.room, "bed")
+        if status == Task.ERROR:
+            raise NotImplementedError()
+        self.phases.put(self.handoff_items)
+
+    def handoff_items(self):
+        # Arrived at patient's bedside
+        status = self.handoff_task.execute()
+        if status == Task.ERROR:
+            raise NotImplementedError()
+        self.phases.put(self.triage_patient)
+
+    def go_idle(self):
+        status = self.idle_task.execute()
+        if status == Task.ERROR:
+            raise NotImplementedError()
+
+    def goal_cb(self, goal):
+        # TODO execute subtasks in order and report status
+        # Nav -> Triage -> Nav -> Stock -> Nav ->
+        #   Handoff -> Idle
+
+        self.room = goal.room
+        self.phases.put(self.go_to_patient)
+
+        while not self.phases.empty():
+            phase = self.phases.get()
+            status = phase()
+
+            if not status:
+                self.server.set_aborted()
+                return
+
+        self.server.set_succeeded()
+
+    def estop_cb(self, msg):
+        self.server.set_aborted()
+
+
+def main():
+    mission_planner = MissionPlanner()
+
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
