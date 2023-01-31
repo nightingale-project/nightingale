3cdbfdf947b2a1f8f43d9388d8a96a25e75ba168
Felix Wong <felix.wongkh@outlook.com>
2023-01-30 23:25:32 -0500

    Add interface client to tasks

--- nightingale_dispatcher/src/nightingale_dispatcher/task.py
+++ nightingale_dispatcher/src/nightingale_dispatcher/task.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
-
+import rospy
+from nightingale_ros_bridge import BridgeConfig, RobotStatus, UserInputs
+from nightingale_msgs.srv import InterfaceCall
 
 class Task:
-    ERROR = -1
-    SUCCESS = 0
+    def __init__(self):
+        self.ERROR = -1
+        self.SUCCESS = 0
+        # client for interface communication
+        self.current_robot_state = RobotStatus.IDLE_HOME
+        self.interface_comms_service_name = BridgeConfig.UPDATE_UI_SERVICE
+
+    def interface_comms_client(self, robot_state):
+        rospy.wait_for_service(self.interface_comms_service_name, InterfaceCall)
+        try:
+            # spin up proxy when needed
+            self.interface_comms_proxy = rospy.ServiceProxy(
+                self.interface_comms_service_name, InterfaceCall
+            )
+            user_input = self.interface_comms_proxy(robot_state)
+            return user_input
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
