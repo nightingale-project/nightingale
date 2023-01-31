9d6e25b875f56c51f6070e960fb2aab4a325d7ac
Felix Wong <felix.wongkh@outlook.com>
2023-01-30 23:41:51 -0500

    Format

--- nightingale_dispatcher/src/nightingale_dispatcher/task.py
+++ nightingale_dispatcher/src/nightingale_dispatcher/task.py
@@ -3,12 +3,11 @@ import rospy
 from nightingale_ros_bridge import BridgeConfig, RobotStatus, UserInputs
 from nightingale_msgs.srv import InterfaceCall
 
+
 class Task:
     def __init__(self):
         self.ERROR = -1
         self.SUCCESS = 0
-        # client for interface communication
-        self.current_robot_state = RobotStatus.IDLE_HOME
         self.interface_comms_service_name = BridgeConfig.UPDATE_UI_SERVICE
 
     def interface_comms_client(self, robot_state):
@@ -21,4 +20,4 @@ class Task:
             user_input = self.interface_comms_proxy(robot_state)
             return user_input
         except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+            print("Service call failed: %s" % e)
