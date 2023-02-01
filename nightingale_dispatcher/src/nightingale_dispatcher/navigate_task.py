4cdcf2a680a84e0a6315be2ad26abe4915dd8f7d
younes <52610118+younesr1@users.noreply.github.com>
2023-01-31 19:07:33 -0500

    define navigate task in mission planner (#29)

--- nightingale_dispatcher/src/nightingale_dispatcher/navigate_task.py
+++ nightingale_dispatcher/src/nightingale_dispatcher/navigate_task.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
 
 import rospy
-
 from nightingale_dispatcher.task import Task
+import actionlib
+from actionlib_msgs.msg import GoalStatus
+from nightingale_msgs.msg import RoomRunnerAction, RoomRunnerGoal
 
 
 class NavigateTask(Task):
-    # Go to position with given context
-    # TODO define parameters
-    def __init__(self, *args, **kwargs):
-        raise NotImplementedError()
+    def __init__(self):
+        action_name = "/nightingale_navigation/room_runner"
+        self.action_client = actionlib.SimpleActionClient(action_name, RoomRunnerAction)
+        rospy.loginfo(f"Waiting for {action_name} action server")
+        if self.action_client.wait_for_server():
+            rospy.loginfo(
+                f"Found the {action_name} action server",
+            )
+        else:
+            rospy.logfatal(
+                f"Failed to find the {action_name} action server",
+            )
 
-    # TODO define inputs
-    def execute(self, *args, **kwargs):
-        # Check if there are intermediate steps to travel to the destination
-        # Call room_runner to drive to pose
-        raise NotImplementedError()
+    def execute(self, room_number, sublocation):
+        goal = RoomRunnerGoal()
+        goal.room_number = room_number
+        goal.sublocation = sublocation
+        self.action_client.send_goal(goal)
+        self.action_client.wait_for_result()
+        return (
+            Task.SUCCESS
+            if self.action_client.get_state() == GoalStatus.SUCCEEDED
+            else Task.ERROR
+        )
