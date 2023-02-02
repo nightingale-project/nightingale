f94d542ea0322674fe21b984ce99924d93bc4bdf
Felix Wong <felix.wongkh@outlook.com>
2023-02-01 20:25:23 -0500

    Test arm length calc

--- nightingale_perception/src/nightingale_perception/pose_test.py
+++ nightingale_perception/src/nightingale_perception/pose_test.py
@@ -2,9 +2,54 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import time
+import math
 
+class Landmarks:
+    # assign the integers to the useful ones
+    NOSE = 0
+    LEFT_EYE = 2
+    RIGHT_EYE = 5
+    LEFT_EAR = 7
+    RIGHT_EAR = 8
+    MOUTH_LEFT = 9
+    MOUTH_RIGHT = 10
+    LEFT_SHOULDER = 11
+    RIGHT_SHOULDER = 12
+    LEFT_ELBOW = 13
+    RIGHT_ELBOW = 14
+    LEFT_WRIST = 15
+    RIGHT_WRIST = 16
+    LEFT_THUMB = 21
+    RIGHT_THUMB = 22
+    LEFT_HIP = 23
+    RIGHT_HIP = 24
+
+class coordinate:
+    def __init__(self, landmark):
+        self.x = landmark.x
+        self.y = landmark.y
+        self.z = landmark.z
+
+def euclid_dist_3d(coord_1, coord_2):
+    # coords -> {'x': num, 'y': num, 'z': num}
+    x_dist = coord_1.x - coord_2.x
+    y_dist = coord_1.y - coord_2.y
+    z_dist = coord_1.z - coord_2.z
+
+    distance = math.sqrt(x_dist**2 + y_dist**2 + z_dist**2) 
+    return distance
+
+def calc_arm_len(thumb_lm, elbow_lm, shoulder_lm):
+    arm_length = 0
+    shoulder_coord = coordinate(shoulder_lm)
+    elbow_coord = coordinate(elbow_lm)
+    thumb_lm = coordinate(thumb_lm)
+    bicep_length = euclid_dist_3d(shoulder_coord, elbow_coord)
+    forearm_length = euclid_dist_3d(elbow_coord, thumb_lm)
+    return bicep_length + forearm_length
 
 def main():
+    lm = Landmarks()
     mp_pose = mp.solutions.pose
     mp_drawing = mp.solutions.drawing_utils
     mp_drawing_styles = mp.solutions.drawing_styles
@@ -25,7 +70,7 @@ def main():
         fps = 1/(new_frame_time-prev_frame_time)
         prev_frame_time = new_frame_time
         fps = str(int(fps))
-        print(fps)
+        #print("fps",fps)
 
         if not success:
           print("Ignoring empty camera frame.")
@@ -37,6 +82,18 @@ def main():
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = pose.process(image)
+        landmarks_list = results.pose_world_landmarks.landmark
+        #print("thumb", landmarks_list[lm.RIGHT_THUMB])
+        #print("eb", landmarks_list[lm.RIGHT_ELBOW])
+        #print("shoulder", landmarks_list[lm.RIGHT_SHOULDER])
+
+        if landmarks_list[lm.RIGHT_THUMB] and landmarks_list[lm.RIGHT_ELBOW] and landmarks_list[lm.RIGHT_SHOULDER]:
+            right_thumb_lm = landmarks_list[lm.RIGHT_THUMB]
+            right_elbow_lm = landmarks_list[lm.RIGHT_ELBOW]
+            right_shoulder_lm = landmarks_list[lm.RIGHT_SHOULDER]
+            right_arm_length = calc_arm_len(right_thumb_lm, right_elbow_lm, right_shoulder_lm)
+            print("Right arm length", right_arm_length)
+        #print(landmarks_list[lm.RIGHT_EYE])
 
         # Draw the pose annotation on the image.
         image.flags.writeable = True
