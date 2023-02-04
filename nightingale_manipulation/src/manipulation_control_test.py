6bfe88db3c370374131ea8c75f03de0af411cd20
RoboHub User <robohub@uwaterloo.ca>
2023-02-04 15:58:57 -0500

    Fix arm runtime errors

--- nightingale_manipulation/src/manipulation_control_test.py
+++ nightingale_manipulation/src/manipulation_control_test.py
@@ -11,16 +11,27 @@ def main(*args, **kwargs):
 
     for i in range(3):
         #man_ctrl.jnt_ctrl.home()
-        man_ctrl.jnt_ctrl.cmd_right_arm(man_ctrl.jnt_ctrl.right_arm_home_joint_values, blocking=False)
-        man_ctrl.jnt_ctrl.right_arm.wait_for_result()
-        
+        rospy.loginfo("Going home")
+        man_ctrl.jnt_ctrl.home()
+#        man_ctrl.jnt_ctrl.cmd_right_arm(man_ctrl.jnt_ctrl.right_arm_home_joint_values)
+        rospy.loginfo("Arrived home")        
+
         rospy.sleep(3)
 
+        rospy.loginfo("Going extended")
         man_ctrl.extend_handoff()
-        #man_ctrl.gpr_ctrl.cmd_right_gripper(man_ctrl.gpr_ctrl.right_open_goal)
+        rospy.loginfo("Arrived handoff")
 
-        #rospy.sleep(1)
-        #man_ctrl.gpr_ctrl.cmd_right_gripper(man_ctrl.gpr_ctrl.right_closed_goal)
+    #    rospy.loginfo("Going gripper open")
+ #       man_ctrl.gpr_ctrl.cmd_right_gripper(man_ctrl.gpr_ctrl.right_open_goal)
+     #   rospy.loginfo("Arrived gripper open")
+
+   #     rospy.sleep(1)
+      #  rospy.loginfo("Going gripper closed")
+  #      man_ctrl.gpr_ctrl.cmd_right_gripper(man_ctrl.gpr_ctrl.right_closed_goal)
+       # rospy.loginfo("Arrived gripper closed")
+
+        rospy.sleep(3)
 
 if __name__ == "__main__":
     main()
