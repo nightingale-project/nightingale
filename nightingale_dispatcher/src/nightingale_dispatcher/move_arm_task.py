76ff04ef5b191d5409edb7d9f6d18c02e8913f0b
younes <yreda@uwaterloo.ca>
2023-02-23 10:25:01 -0500

    fixed up cartesian control of arms

--- nightingale_dispatcher/src/nightingale_dispatcher/move_arm_task.py
+++ nightingale_dispatcher/src/nightingale_dispatcher/move_arm_task.py
@@ -9,8 +9,20 @@ from nightingale_manipulation.manipulation_action_client import ManipulationCont
 class MoveArmTask(Task):
     def __init__(self):
         self.manipulation = ManipulationControl()
+        if not self.manipulation.home():
+            rospy.logfatal("MoveArmTask: Failed to home arms. This is unrecoverable")
 
-    def execute(self, end_pose):
-        assert len(end_pose) is 7
-        self.manipulation.jnt_ctrl.cmd_right_arm(end_pose)
-        return TaskCodes.SUCCESS
+    def extend_restock(self):
+        result = self.manipulation.extend_restock()
+        assert type(result) is bool
+        return Task.SUCCESS if result else Task.ERROR
+
+    def retract_right_arm(self):
+        result = self.manipulation.home()
+        assert type(result) is bool
+        return Task.SUCCESS if result else Task.ERROR
+
+    def extend_handoff(self, point):
+        result = self.manipulation.extend_handoff(point)
+        assert type(result) is bool
+        return Task.SUCCESS if result else Task.ERROR
