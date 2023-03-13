55d3b40953f60fb494ffc31a152fb2bf8e947673
le2tang <le2tang@uwaterloo.ca>
2023-03-13 16:58:24 -0400

    Add calibration and noise model for collision detection

--- nightingale_payload/src/main.py
+++ nightingale_payload/src/main.py
@@ -11,9 +11,59 @@ from std_msgs.msg import Bool
 from urdf_parser_py.urdf import Robot
 
 
+class SensorCalibration:
+    def __init__(self, record_func):
+        self.xdata = []
+        self.ydata = []
+
+        self.record_func = record_func
+
+        self.record_sub = rospy.Subscriber("analysis/record", Bool, self.record)
+        self.clear_sub = rospy.Subscriber("analysis/clear", Bool, self.clear)
+        self.analyze_sub = rospy.Subscriber("analysis/analyze", Bool, self.analyze)
+
+        self.weights = None
+
+    def record(self, msg):
+        x, y = self.record_func()
+
+        self.xdata.append(list(x))
+        self.ydata.append(list(y))
+
+        rospy.loginfo(f"Recorded:\nx={self.xdata}\ny={self.ydata}")
+
+    def clear(self, msg):
+        self.xdata = []
+        self.ydata = []
+
+        rospy.loginfo("Cleared")
+
+    def analyze(self, msg):
+        x = np.array(self.xdata)
+        y = np.array(self.ydata)
+        N, M = x.shape
+
+        rospy.loginfo(f"Analyzed:\nx={x}\ny={y}")
+
+        weights = np.zeros((7, 2))
+
+        for idx in range(M):
+            X = np.stack((x[:, idx], np.ones(N))).T
+            Y = y[:, idx]
+
+            rospy.loginfo(f"{X} {Y}")
+            weights[idx, :] = np.linalg.inv(X.T @ X) @ X.T @ Y
+
+        rospy.loginfo(f"weights: {weights}")
+
+        self.weights = weights
+
+        np.savez("/home/lyndon/Documents/mte_4812/data.npz", x=x, y=y)
+
+
 class PayloadEstimator:
     GRAVITATIONAL_ACCELERATION = 9.8  # [m/sec^2]
-    PAYLOAD_DETECTION_THRESHOLD = 1  # [kg]
+    PAYLOAD_DETECTION_THRESHOLD = 50  # std dev
 
     def __init__(self, arm_side):
         rospy.init_node("payload_estimator_node")
@@ -49,6 +99,94 @@ class PayloadEstimator:
         self.gravity_direction = np.zeros(3)
         self.gravity_forces = np.zeros(6)
 
+        self.weights = {
+            "right": np.zeros((self.dof, 2)),
+            "left": np.array(
+                [
+                    [-2.44161661e00, -8.80378258e00],
+                    [-2.11674602e00, -3.00928765e00],
+                    [-3.19820241e00, -3.09657843e00],
+                    [-3.93661567e00, 2.14572217e00],
+                    [-2.28678270e00, -7.84129984e00],
+                    [-7.25424816e00, -6.86924505e-01],
+                    [-2.16260351e-14, 1.31909042e-14],
+                ]
+            ),
+        }
+        self.error_models = {
+            "right": np.zeros((self.dof, self.dof)),
+            "left": np.array(
+                [
+                    [
+                        5.68075404e01,
+                        -1.24970088e01,
+                        2.49631265e00,
+                        3.24242928e00,
+                        1.80881417e01,
+                        -5.92326271e-01,
+                        -9.34355742e-15,
+                    ],
+                    [
+                        -1.24970088e01,
+                        2.96429465e01,
+                        -1.58984130e-01,
+                        -2.08735398e00,
+                        -8.15410739e00,
+                        1.20005465e-01,
+                        -3.14085321e-15,
+                    ],
+                    [
+                        2.49631265e00,
+                        -1.58984130e-01,
+                        5.55697289e00,
+                        7.44006773e-01,
+                        5.52154569e-01,
+                        1.74704528e-01,
+                        -6.65731777e-15,
+                    ],
+                    [
+                        3.24242928e00,
+                        -2.08735398e00,
+                        7.44006773e-01,
+                        9.03829531e-01,
+                        4.42269859e-01,
+                        2.41429762e-02,
+                        -2.66232176e-16,
+                    ],
+                    [
+                        1.80881417e01,
+                        -8.15410739e00,
+                        5.52154569e-01,
+                        4.42269859e-01,
+                        1.10720364e01,
+                        -1.64741943e-01,
+                        -2.65425581e-15,
+                    ],
+                    [
+                        -5.92326271e-01,
+                        1.20005465e-01,
+                        1.74704528e-01,
+                        2.41429762e-02,
+                        -1.64741943e-01,
+                        2.80167501e-02,
+                        1.20682787e-16,
+                    ],
+                    [
+                        -9.34355742e-15,
+                        -3.14085321e-15,
+                        -6.65731777e-15,
+                        -2.66232176e-16,
+                        -2.65425581e-15,
+                        1.20682787e-16,
+                        1.41876623e-29,
+                    ],
+                ]
+            ),
+        }
+
+        self.iter = 0
+        self.iter_decimation = 10
+
         self.forces = np.zeros(6)
         self.last_forces = np.zeros(6)
         self.filter_coeff = 1
@@ -57,79 +195,44 @@ class PayloadEstimator:
         self.mass_idx = 0
         self.masses = np.zeros(self.mass_num)
 
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.lookup_gravity_tf()
+
+        self.analysis = SensorCalibration(self.calibration_record_cb)
+        self.measured_torques = np.zeros(7)
+        self.computed_torques = np.zeros(7)
+
         self.payload_pub = rospy.Publisher(
             f"/nightingale/payload", Payload, queue_size=10
         )
 
-        self.tf_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-
-        self.lookup_gravity_tf()
-
         self.joint_state_sub = rospy.Subscriber(
-            "/joint_states", JointState, self.joint_state_cb
+            f"/movo/{self.arm_side}_arm/joint_states", JointState, self.joint_state_cb
         )
 
-        self.analysis_num = 1000
-        self.analysis_dec = 5
-        self.analysis_idx = 0
-        self.analysis_forces = np.zeros((6, self.analysis_num))
+    def calibration_record_cb(self):
+        return (self.measured_torques, self.computed_torques)
 
-        self.analysis_sub = rospy.Subscriber(
-            "analyze_ee_forces", Bool, self.analyze_ee_forces
+    def scale_torques(self, torques):
+        return (
+            self.weights[self.arm_side][:, 0] * np.array(torques)
+            + self.weights[self.arm_side][:, 1]
         )
 
     def joint_state_cb(self, msg):
-        self.lookup_tf()
+        if self.iter == self.iter_decimation - 1:
+            self.lookup_tf()
 
-        self.get_jacobian()
-        self.get_gravity()
+            self.get_jacobian()
+            self.get_gravity()
 
-        # 4: right_shoulder_pan_joint
-        # 5: right_shoulder_lift_joint
-        # 6: right_arm_half_joint
-        # 7: right_elbow_joint
-        # 8: right_wrist_spherical_1_joint
-        # 9: right_wrist_spherical_2_joint
-        # 10: right_wrist_3_joint
-        self.compute_ee_forces(msg.effort[4:11])
+            torques = self.scale_torques(msg.effort[: self.dof])
+            self.compare_torques(torques)
 
-        self.compare_torque(msg.effort[4:11])
-        # rospy.loginfo(f"pos: {np.round(msg.position[4:11], 2)}")
-        # rospy.loginfo(f"vel: {np.round(msg.velocity[4:11], 2)}")
-
-        self.analysis_idx += 1
-        if self.analysis_idx % self.analysis_dec == 0:
-            self.analysis_forces[
-                :, (self.analysis_idx // self.analysis_dec) % self.analysis_num
-            ] = np.concatenate((self.forces, np.zeros((3, 1)))).flatten()
-            if self.analysis_idx // self.analysis_dec == self.analysis_num:
-                rospy.loginfo("Ready to analyze")
-                self.analysis_idx = 0
-
-    def analyze_ee_forces(self, msg):
-        mean = np.mean(self.analysis_forces, axis=1)
-        covariance = np.cov((self.analysis_forces.T - mean).T)
-
-        evals, evecs = np.linalg.eig(covariance)
-        principal_axes = np.sqrt(evals) * evecs
-
-        timestamp = int(rospy.get_time())
-        filename = f"/home/lyndon/Documents/mte_4812/arm_force/data/arm_force_analysis_{timestamp}.npz"
-        np.savez(
-            filename,
-            forces=self.analysis_forces,
-            gravity_direction=self.gravity_direction,
-            mean=mean,
-            covariance=covariance,
-            evals=evals,
-            evecs=evecs,
-            principal_axes=principal_axes,
-        )
-        rospy.loginfo(f"Saved to {filename}")
-
-        rospy.loginfo(mean)
-        rospy.loginfo(covariance)
+            self.iter = 0
+        else:
+            self.iter += 1
 
     def lookup_gravity_tf(self):
         try:
@@ -164,7 +267,7 @@ class PayloadEstimator:
 
         for idx in range(self.dof + 1):
             from_link = f"{self.arm_side}_{self.joint_link_suffixes[0]}"
-            to_link = f"{self.arm_side}_{self.joint_link_suffixes[idx]}"
+            to_link = f"{self.arm_side}_{self.joint_link_suffixes[idx + 1]}"
             try:
                 link_transform = self.tf_buffer.lookup_transform(
                     from_link, to_link, rospy.Time()
@@ -178,8 +281,6 @@ class PayloadEstimator:
                         link_transform.transform.rotation.w,
                     ]
                 )
-                if idx == 1:
-                    rospy.loginfo(f"{from_link}->{to_link}:\n{np.round(rot_mat, 2)}")
                 rotation_axes[idx, :] = rot_mat[:3, 2]
 
                 translations[idx, :] = [
@@ -227,51 +328,37 @@ class PayloadEstimator:
 
         self.gravity_forces = gravity_forces
 
-    def compare_torque(self, torques):
-        rospy.loginfo(f"\n{np.round((self.jacobian[:3, :]).T, 2)}")
-
-        computed_torque = self.jacobian.T @ self.gravity_forces
-        error = np.array(torques) - computed_torque
-        error_norm = np.sum(np.abs(error))
+    def compare_torques(self, torques):
+        computed_torques = self.jacobian[:3, :].T @ self.gravity_forces[:3]
+        error = np.array(torques) - computed_torques
+        error_norm = np.sqrt(np.sum(np.square(error)))
 
         # rospy.loginfo(
-        #     f"{np.round(torques, 2)} {np.round(computed_torque)} {error_norm}"
-        # )
-        # rospy.loginfo(f"{np.round(error, 4)} {error_norm}")
-
-    def compute_ee_forces(self, torques):
-        forces = (
-            np.linalg.inv(self.jacobian @ self.jacobian.T) @ self.jacobian @ torques
-        )  # - self.gravity_forces
-        forces_base_link = self.base_link_rotation.T @ np.reshape(forces[:3], (3, 1))
-
-        self.forces = self.last_forces + self.filter_coeff * (
-            forces_base_link - self.last_forces
-        )
-        self.last_forces = forces_base_link
-
-        payload_mass = (
-            np.dot(self.forces[:3].flatten(), self.gravity_direction)
-            / self.GRAVITATIONAL_ACCELERATION
-        )
-
-        # rospy.loginfo(
-        #     f"{np.round(forces_base_link, 3).flatten()} {np.sqrt(np.sum(np.square(forces_base_link)))} {payload_mass}"
+        #     f"{np.round(torques, 2)} {np.round(computed_torques)} {np.round(error_norm)}"
         # )
 
-        payload = Payload()
-        payload.mass = payload_mass
-        payload.detected = payload_mass > self.PAYLOAD_DETECTION_THRESHOLD
+        # if self.analysis.weights is not None:
+        #     calibrated_torques = (
+        #         np.array(torques) * self.analysis.weights[:, 0]
+        #         + self.analysis.weights[:, 1]
+        #     )
+        #     rospy.loginfo(
+        #         f"{np.round(calibrated_torques, 2)} {np.round(computed_torques)}"
+        #     )
 
-        self.masses[self.mass_idx] = payload_mass
-        self.mass_idx = (self.mass_idx + 1) % self.mass_num
+        self.measured_torques = torques
+        self.computed_torques = self.jacobian[:3, :].T @ self.gravity_forces[:3]
 
-        self.avg_mass = np.mean(self.masses)
-        self.var_mass = np.var(self.masses)
-
-        self.payload_pub.publish(payload)
+        error = (self.measured_torques - self.computed_torques).reshape((self.dof, 1))
+        error_distance = np.sqrt(
+            error[:6, 0].T
+            @ np.linalg.inv(self.error_models[self.arm_side][:6, :6])
+            @ error[:6, 0]
+        )
+        if np.any(error_distance > self.PAYLOAD_DETECTION_THRESHOLD):
+            rospy.logwarn(f"COLLISION_DETECTED: {error_distance}")
 
 
 if __name__ == "__main__":
-    payload_estimator = PayloadEstimator("right")
+    payload_estimator = PayloadEstimator("left")
     rospy.spin()
