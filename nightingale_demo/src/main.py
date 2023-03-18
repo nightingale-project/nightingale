605dcd73aa516640827676eb51118699699b954f
le2tang <le2tang@uwaterloo.ca>
2023-03-18 11:42:43 -0400

    Add callback for buttons

--- nightingale_demo/src/main.py
+++ nightingale_demo/src/main.py
@@ -1,9 +1,14 @@
 # /usr/bin/env python3
 
+import actionlib
 import rospy
 
-from control_msgs.msg import FollowJointTrajectoryGoal
-from nightingale_ros_bridge.bridge_interface_config import BridgeConfig, RobotStatus
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+from nightingale_ros_bridge.bridge_interface_config import (
+    BridgeConfig,
+    RobotStatus,
+    UserInput,
+)
 from nightingale_manipulation import ManipulationControl
 from std_msgs.msg import Bool, String
 
@@ -12,10 +17,9 @@ class SymposiumDemo:
     def __init__(self):
         rospy.init_node("symposium_demo_node")
 
-        self.arm_pub = rospy.Publisher(
+        self.arm_action_client = actionlib.SimpleActionClient(
             "/movo/right_arm_controller/follow_joint_trajectory",
-            FollowJointTrajectoryGoal,
-            queue_size=2,
+            FollowJointTrajectoryAction,
         )
 
         self.right_collision_sub = rospy.Subscriber(
@@ -37,7 +41,7 @@ class SymposiumDemo:
             self.enable_left_collision_cb,
         )
         self.collision_screen_pub = rospy.Publisher(
-            BridgeConfig.UPDATE_UI_SERVICE, String, queue_size=10
+            BridgeConfig.ROBOT_STATUS_TOPIC, String, queue_size=10
         )
 
         self.screen_button_sub = rospy.Subscriber(
@@ -57,6 +61,21 @@ class SymposiumDemo:
 
     def enable_left_collision_cb(self, msg):
         self.enable_left_collision = msg.data
+
+    def screen_button_cb(self, msg):
+        # goal = FollowJointTrajectoryGoal()
+        # goal.path_tolerance = 1
+        # goal.goal_tolerance = 1
+        # goal.goal_time_tolerance = rospy.Duration(10)
+
+        if msg.data == UserInput.START_EXTEND_ARM:
+            rospy.loginfo(f"Start extending arm")
+        #     goal.trajectory = []
+        elif msg.data == UserInput.START_RETRACT_ARM:
+            rospy.loginfo(f"Start retracting arm")
+        #     goal.trajectory = []
+
+        # self.arm_action_client.send_goal(goal)
 
 
 if __name__ == "__main__":
